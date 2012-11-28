5e27b2fae06ba41f4e6fbd9f155c0487660f5f13
Forrest <forrest.shi@ef.com>
2012-11-28 15:50:19 +0800

    Initial Commit

---
+++ TestJenkins/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TestJenkins
+//
+//  Created by Forrest Shi on 11/28/12.
+//  Copyright (c) 2012 Forrest Shi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ TestJenkins/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  TestJenkins
+//
+//  Created by Forrest Shi on 11/28/12.
+//  Copyright (c) 2012 Forrest Shi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
